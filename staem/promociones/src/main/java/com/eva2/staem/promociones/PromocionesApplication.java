8119bbcb57b6eb6b08ab99255afca7cfd5e7a4d4
Hik4ru23 <gon.yanez@duocuc.cl>
2026-06-20 09:42:04 -0400

    conexion de todos los microservicios a eureka

--- staem/promociones/src/main/java/com/eva2/staem/promociones/PromocionesApplication.java
+++ staem/promociones/src/main/java/com/eva2/staem/promociones/PromocionesApplication.java
@@ -2,10 +2,14 @@ package com.eva2.staem.promociones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(scanBasePackages = "com.eva2.staem")
+@EnableDiscoveryClient
 public class PromocionesApplication {
     public static void main(String[] args) {
         SpringApplication.run(PromocionesApplication.class, args);
     }
 }
+
+
 
--- staem/usuarios/src/main/java/com/eva2/staem/usuarios/UsuariosApplication.java
+++ staem/usuarios/src/main/java/com/eva2/staem/usuarios/UsuariosApplication.java
@@ -2,9 +2,13 @@ package com.eva2.staem.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication(scanBasePackages = "com.eva2.staem")
+@EnableDiscoveryClient
 public class UsuariosApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsuariosApplication.class, args);
     }
 }
+
